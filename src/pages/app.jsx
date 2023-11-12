06afc09eaab38e92bf76e86997b180ef1bb943cb
Andrea Carmona <andrea@carmonaweb.com>
2023-11-12 16:54:28 -0500

    finish layout

---
+++ src/pages/app.jsx
@@ -0,0 +1,14 @@
+import Layout from "../template/layout";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
